79cd282eac8b3a1200686937f94a320153bc4b36
Ian Harwell <sirharwell@gmail.com>
2018-06-11 23:00:18 -0600

    models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+    # Validations
+    validates_presence_of :name
+
+    # Assocations
+    has_many :recipe_ingredients, dependent: :destroy
+    has_many :ingredients, through: :recipe_ingredients
 end

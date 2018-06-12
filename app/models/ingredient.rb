beea91c26ce72a81b80e3adf5aa0c90cc6d329c5
Ian Harwell <sirharwell@gmail.com>
2018-06-11 23:17:10 -0600

    ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,10 @@
 class Ingredient < ApplicationRecord
+    # Validations
+    validates_presence_of :name
+    # This ensures that we do not have duplicate of the same named ingredients in our database
+    validates_uniqueness_of :name, case_sensitive: false
+
+    # Associations
+    has_many :recipe_ingredients, dependent: :destroy
+    has_many :recipes, through: :recipe_ingredients
 end

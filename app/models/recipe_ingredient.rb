2471adc60b0e8543bb1f8b7725881705824a2b0d
Ian Harwell <sirharwell@gmail.com>
2018-06-13 23:08:35 -0600

    recipe-ingredient

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,13 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
-end
+  # This table will look something like this
+  # has_many :through is good here because we only have to create an ingredient once
+  # once that ingredient is created we create a new recipe record and a new recipe_ingredient record
+  # this links each new recipe up with the ingredient that is already in the database
+
+  # recipe_id | ingredient_id
+  #      1          1
+  #      1          3
+  #      2          1
+  #      10         3

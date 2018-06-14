8b80ab7bdf7a2b2944d82c9cd887923ee4c7792f
Ian Harwell <sirharwell@gmail.com>
2018-06-13 23:10:50 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'recipes#index'
+
+    resources :ingredients
+    resources :recipes
+    resources :recipe_ingredients, only: [:new, :create, :destroy]
 end
